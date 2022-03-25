da830bc62e559b267b90aff4cc85a827701585af
Hritik thapa <57740801+HritikThapa7@users.noreply.github.com>
2022-03-25 07:43:15 +0545

    MongoDB URI updated

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from .extensions import mongo
 
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb+srv://HritikThapa7:infoxdb@cluster0.gechm.mongodb.net/infoxdb?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = 'mongodb+srv://<db_username>:<password_to_username>@cluster0.gechm.mongodb.net/<db_name>?retryWrites=true&w=majority'
 mongo.init_app(app)
 
 
